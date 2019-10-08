d89b6b5372aff0bfb2707ef1c12c0c3dc8ac3ddd
abudimirov <alexys077@gmail.com>
2019-10-08 10:39:40 +0300

    added menu and several pages

--- server.js
+++ server.js
@@ -8,10 +8,30 @@ const server = http.createServer(function (request, response){
    if (request.url == '/style.css') {
       const css = fs.readFileSync('style.css', 'utf8');
       response.end(css);
-   } else {
+   } else if (request.url == '/profile')  {
+      const profile = fs.readFileSync('profile.html', 'utf8');
+      response.end(profile);
+   } else if (request.url == '/login')  {
+      const login = fs.readFileSync('login.html', 'utf8');
+      response.end(login);
+   }
+   else if (request.url == '/registration')  {
+      const registration = fs.readFileSync('registration.html', 'utf8');
+      response.end(registration);
+   }
+   else if (request.url == '/leaders')  {
+      const leaders = fs.readFileSync('leaders.html', 'utf8');
+      response.end(leaders);
+   }
+   else if (request.url == '/about')  {
+      const about = fs.readFileSync('about.html', 'utf8');
+      response.end(about);
+   }
+   else if (request.url == '/')  {
       const index = fs.readFileSync('index.html', 'utf8');
       response.end(index);
    }
+
 });
 
 console.log('port = ', process.env.PORT);
